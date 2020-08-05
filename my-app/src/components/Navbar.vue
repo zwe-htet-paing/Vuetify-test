669060d1a4bf15fb4474130c94076e024b285a51
zwe-htet-paing <zwehtetpaing123@gmail.com>
2020-08-06 01:35:19 +0630

    Add navbar and navigation-drawer

---
+++ my-app/src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav>
+
+    <v-toolbar flat app>
+      <v-app-bar-nav-icon @click="drawer = !drawer" class="grey--text"></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">Todo</span>
+        <span>Ninja</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat color="grey">
+        <span>Sign Out</span>
+        <v-icon right>exit_to_app</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-navigation-drawer app v-model="drawer" class="indigo">
+      <p>test</p>
+    </v-navigation-drawer>
+
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
