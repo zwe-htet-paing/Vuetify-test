22402a44f25f5ad0c2cf7926aa3012cf0517fd2c
zwe-htet-paing <zwehtetpaing123@gmail.com>
2020-08-07 17:10:07 +0630

    Successfully fix date error and add firebase databse

--- my-app/src/components/Popup.vue
+++ my-app/src/components/Popup.vue
@@ -14,14 +14,12 @@
           <!-- form start -->
           <v-form class="px-3" ref="form">
             <v-text-field
-
               v-model="title"
               label="Title"
               prepend-icon="mdi-folder"
               :rules="inputRules"
             ></v-text-field>
             <v-textarea
-             
               v-model="content"
               label="Information"
               prepend-icon="mdi-file-edit"
@@ -29,30 +27,39 @@
             ></v-textarea>
             <!-- Date picker start -->
             <v-container>
-              <v-menu
-                offset-y
-                v-model="menu"
-                :close-on-content-click="false"
-                max-width="290"
-              >
-                <template v-slot:activator="{ on, attrs }">
-                  <v-text-field
-        
-                    v-bind="attrs"
-                    v-on="on"
-                    v-model="formattedDate"
-                    clearable
-                    hint="MM/DD/YYYY format"
-                    label="Due date"
-                    prepend-icon="mdi-calendar-range"
-                    @click:clear="date = null"
-                    :rules="inputRules"
+              <v-row>
+                <v-col> 
+                  <v-menu
+                    v-model="menu"
+                    :close-on-content-click="false"
+                    :nudge-right="40"
+                    transition="scale-transition"
+                    offset-y
+                    min-width="290px"
+                    max-width="290px"
                   >
-                  </v-text-field>
-                </template>
-                <v-date-picker v-model="date" @change="menu = false" clearable>
-                </v-date-picker>
-              </v-menu>
+                    <template v-slot:activator="{ on }">
+                      <v-text-field
+                        v-model="selectedDate"
+                        label="Due date"
+                        prepend-icon="mdi-calendar-range"
+                        readonly
+                        v-on="on"
+                        clearable
+                        @click:clear="date = null"
+                        :rule="inputRules"
+                      >
+                      </v-text-field>
+                    </template>
+                    <v-date-picker
+                      v-model="date"
+                      @input="closeDateMenu"
+                      clearable
+                    >
+                    </v-date-picker>
+                  </v-menu>
+                </v-col>
+              </v-row>
             </v-container>
             <v-spacer></v-spacer>
             <v-btn text @click="submit" class="success mx-0 mt-3"
@@ -70,48 +77,78 @@
 </template>
 
 <script>
+import moment from "moment";
+//import  format from 'date-fns'
+//import db from '@/fb';
+
 export default {
-  data: (vm) => ({
+  data: () => ({
     dialog: false,
     title: "",
     content: "",
     menu: false,
     date: null,
-    //date: new Date().toISOString().substr(0, 10),
-    dateFormatted: vm.formatDate(new Date().toISOString().substr(0, 10)),
+    selectedDate: null,
+    //date:  new Date().toISOString().substr(0, 10),
+    //dateFormatted: vm.formatDate(new Date().toISOString().substr(0, 10)),
     inputRules: [
       (v) => !!v || "This field is required.",
       (v) => v.length >= 3 || "Minimum length is 3 characters",
     ],
   }),
+
   methods: {
-    
     submit() {
-      if (this.$refs.form.validate()) { //form validation
-        console.log(this.title, this.content);
+      if(this.$refs.form.validate()) {
+        // const project = { 
+        //   title: this.title,
+        //   content: this.content,
+        //   due: this.selectedDate,
+        //   person: 'The Net Ninja',
+        //   status: 'ongoing'
+        // }
+        // db.collection('projects').add(project).then(() => {
+        //   console.log('added to db')
+        // })
       }
     },
     clear() {
-      (this.title = ""), (this.content = "");
-      this.data = "";
+      (this.title = ""), (this.content = ""), (this.data = "");
     },
-    formatDate(date) {
-      if (!date) return null;
+    closeDateMenu() {
+      this.menu = false;
+      //this.selectedDate= this.date ? moment(this.date).format('dddd, MMMM Do YYYY') : '';
+      this.selectedDate = this.date
+        ? moment(this.date).format("Do MMM YYYY")
+        : "";
+    },
+    //   formatDate(date) {
+    //     if (!date) return null;
 
-      const [year, month, day] = date.split("-");
-      return `${month}/${day}/${year}`;
-    },
-    parseDate(date) {
-      if (!date) return null;
+    //     const [year, month, day] = date.split("-");
+    //     return `${month}/${day}/${year}`;
+    //   },
+    //   parseDate(date) {
+    //     if (!date) return null;
 
-      const [month, day, year] = date.split("/");
-      return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
-    },
+    //     const [month, day, year] = date.split("/");
+    //     return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
+    //   },
   },
   computed: {
-    formattedDate() {
-      return this.formatDate(this.date);
+    // formattedDate() {
+    //   return this.formatDate(this.date);
+    // },
+    formattedDue() {
+      console.log(this.date);
+      return this.date
+        ? moment(this.date).format(this.date, "Do MMM YYYY")
+        : "";
     },
+
+    // computedDateFormattedDatefns () {
+    //     return this.date ? format(this.date, 'EEEE, MMMM do yyyy') : ''
+    //   },
   },
 };
 </script>
